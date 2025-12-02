c5efd305367a7b097206c09dc628fdc752941b4b
krgoodnews <krgoodnews@gmail.com>
2025-12-02 14:59:48 +0900

    begin: 🎉 Project Init

---
+++ UniUIKit/UniUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniUIKit
+//
+//  Created by Goodnews on 12/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
